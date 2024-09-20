b938c7c1b872a7e2670cb196b5178e8adfbb7bdd
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-20 11:11:21 +0200

    gotowe

--- 5M03konGraWkosci/Program.cs
+++ 5M03konGraWkosci/Program.cs
@@ -4,26 +4,60 @@
     internal class Program
     {
         private static int[] tabLos = new int[10];
-        private static int[] tabwynik = new int[7];
-        //liczba wylosowanych konkretnych wartości
+        
         private static int suma = 0;
+        private static int liczbaRzutów = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("");
-            wylosujTablice();
+            
+            
+            string pytanie;
+            do
+            {
+                wylosujTablice();
+                wypiszTablice();
+                Console.WriteLine($"Liczba uzyskanych punktów: {obliczPunkty()}");
+                Console.WriteLine("Czy jeszcze raz? (t/n)");
+                pytanie = Console.ReadLine();
+            }while (pytanie == "t");
+            
+        }
+
+        private static int obliczPunkty()
+        {
+            int[] tabwynik = new int[7];
+            for(int i=0; i<7; i++)
+                tabwynik[i] = 0;
+            //liczba wylosowanych konkretnych wartości
+            for(int i = 0; i < liczbaRzutów; i++)
+                tabwynik[tabLos[i]]++;
+            //foreach(int i in tabwynik)
+            //    Console.WriteLine(i);
+            int suma = 0;
+            for (int i = 1; i <= 6; i++)
+                if (tabwynik[i] > 1)
+                    suma += tabwynik[i] * i;
+            return suma;
         }
 
 
         private static void wylosujTablice()
         {
-            int liczbaRzutów;
             do
             {
                 Console.WriteLine("ile kostek chcesz rzucić? (3-10)");
                 liczbaRzutów = int.Parse(Console.ReadLine());
             } while (liczbaRzutów < 3 || liczbaRzutów > 11);
-            Console.WriteLine($"rzucam {liczbaRzutów}");
-            
+            //Console.WriteLine($"rzucam {liczbaRzutów}");
+            Random r = new Random();
+            for (int i = 0; i < liczbaRzutów; i++)
+                tabLos[i] = r.Next(1, 7);
+        }
+        private static void wypiszTablice()
+        {
+            for (int i = 0; i < liczbaRzutów; i++)
+                Console.WriteLine($"Kostka {i + 1}: {tabLos[i]}");
+
         }
     }
 }
